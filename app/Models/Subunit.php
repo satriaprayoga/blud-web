9ba712b6168f9bcb3c5346bdf6f37a6747449ad2
satriaprayoga <satria.prayoga@gmail.com>
2022-07-13 20:18:59 +0700

    5th commit

--- app/Models/Subunit.php
+++ app/Models/Subunit.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Unit extends Model
+class Subunit extends Model
 {
     use HasFactory;
-    
-    public function subunits(){
-        return $this->hasMany(Subunit::class);
+
+    public function unit(){
+        return $this->belongsTo(Unit::class);
     }
 }
---
+++ database/migrations/2022_07_13_125731_create_subunits_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subunits', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('kode');
+            $table->string('singkatan');
+            $table->string('nama_bend');
+            $table->string('nip_bend');
+            $table->string('jabatan_bend');
+            $table->string('nama_sptjm');
+            $table->string('nip_sptjm');
+            $table->string('jabatan_sptjm');
+            $table->string('nama_sp2b');
+            $table->string('nip_sp2b');
+            $table->string('jabatan_sp2b');
+            $table->foreignId('unit_id')->constrained('units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subunits');
+    }
+};
