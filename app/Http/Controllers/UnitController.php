afb12d424419c144642375a50eff542bd04f0156
satriaprayoga <satria.prayoga@gmail.com>
2022-07-13 19:56:01 +0700

    4th commit

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -2,9 +2,119 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UnitController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $units=Unit::all();
+        return response()->json(['units'=>$units]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated=$this->unitValidator($request);
+        if($validated->fails()){
+            return response()->json(['errors'=>$validated->errors()],422);
+        }
+        $newUnit=Unit::create([
+            'nama'=>$request->nama,
+            'kode'=>$request->kode,
+            'singkatan'=>$request->singkatan,
+            'lokasi'=>$request->lokasi,
+            'nama_kepala'=>$request->nama_kepala,
+            'nip_kepala'=>$request->nip_kepala,
+            'jabatan_kepala'=>$request->jabatan_kepala
+        ]);
+        return response()->json([
+            'unit'=>$newUnit,
+            'message'=>'Unit baru berhasil dibuat'
+        ],200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Unit  $unit
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $unit=Unit::find($id);
+        if($unit==null){
+            return response()->json(['message'=>'Unit tidak ditemukan'],404);
+        }
+        return response()->json(['unit'=>$unit]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Unit  $unit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        $unit=Unit::find($id);
+        if($unit==null){
+            return response()->json(['message'=>'Unit tidak ditemukan'],404);
+        }
+        $unit->nama=$request->nama;
+        $unit->kode=$request->kode;
+        $unit->singkatan=$request->singkatan;
+        $unit->lokasi=$request->lokasi;
+        $unit->nama_kepala=$request->nama_kepala;
+        $unit->nip_kepala=$request->nip_kepala;
+        $unit->jabatan_kepala=$request->jabatan_kepala;
+        $unit->save();
+        
+        return response()->json(['unit'=>$unit,'message'=>'Unit berhasil diupdate'],200);
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Unit  $unit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $unit=Unit::find($id);
+        if($unit==null){
+            return response()->json(['message'=>'Unit tidak ditemukan'],404);
+        }
+        $unit->destroy();
+        return response()->json(['message'=>'Unit berhasil dihapus'],400);
+    }
+
+    protected function unitValidator(Request $request){
+        return Validator::make($request->all(),[
+            'nama'=>'required|string|max:255',
+            'kode'=>'required|string|max:255',
+            'singkatan'=>'required|string|max:255',
+            'lokasi'=>'required|string|max:255',
+            'nama_kepala'=>'required|string|max:255',
+            'nip_kepala'=>'required|string|max:255',
+            'jabatan_kepala'=>'required|string|max:255'
+        ]);
+    }
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UnitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/register',[AuthController::class,'register']);
+Route::apiResource('units',UnitController::class);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
