4a986a1c451a3ed3c1ff7e071cfda68679ee8da6
satriaprayoga <satria.prayoga@gmail.com>
2022-07-18 22:55:16 +0700

    it12nd commt

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,9 @@
  * includes React and other helpers. It's a great starting point while
  * building robust, powerful web applications using React + Laravel.
  */
-
-require('./bootstrap');
+//import './bootstrap';
+import './components/Example';
+//require('./bootstrap');
 
 /**
  * Next, we will create a fresh React component instance and attach it to
@@ -12,4 +13,4 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/Example');
+//require('./components/Example');
