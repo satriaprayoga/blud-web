4663228d01db0ec59379af775aeb542e93ba985a
satriaprayoga <satria.prayoga@gmail.com>
2022-07-29 16:20:52 +0700

    pk

--- resources/js/views/accounts/Accounts.jsx
+++ resources/js/views/accounts/Accounts.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import MainCard from '../../ui-component/cards/MainCard'
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { Box, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
 import { useState } from 'react'
 import api from '../../utils/api';
+import { SearchOutlined } from '@mui/icons-material'
 
 const Accounts = ({ report }) => {
 
-    const [induk,setInduk]=useState('')
-    const [accounts,setAccounts]=useState([]);
+    const [induk, setInduk] = useState('')
+    const [accounts, setAccounts] = useState([]);
 
-    const handleChange= async (event)=>{
+    const handleChange = async (event) => {
         try {
             setInduk(event.target.value);
-            let response=await api.get(`accounts/report/${report}/type/${event.target.value}`);
+            let response = await api.get(`accounts/report/${report}/type/${event.target.value}`);
             setAccounts(response.data.accounts);
+            console.log(response.data.accounts);
         } catch (error) {
-            
+
         }
     }
 
@@ -40,6 +42,17 @@ const Accounts = ({ report }) => {
                     </Select>
                 </FormControl>
             }>
+            <Grid container>
+                <Grid item xs={4} style={{ display: "flex", justifyContent: "flex-start" }}>
+                    <Typography variant='h3'>{induk.length === 0 ? 'Semua Akun' : induk}</Typography>
+                </Grid>
+                <Grid item xs={8} style={{ display: "flex", justifyContent: "flex-end" }}>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <SearchOutlined />
+                        <TextField id="input-with-sx" label="Cari..." variant="standard" />
+                    </Box>
+                </Grid>
+            </Grid>
 
         </MainCard>
     )
